8803772bfc168f058281ec842b48f4d6da9bc76d
BH267 <hassanbenydir22@gmail.com>
2025-04-23 09:22:21 +0100

    fixed: now can update the variable and add an empty variable

--- builtins/export.c
+++ builtins/export.c
@@ -12,18 +12,35 @@
 
 #include "bins.h"
 
+int	expars(char *str)
+{
+	if (!hb_isalpha(str[0]) && str[0] != '_' && str[0] != '?')
+		return (1);
+	return (0);
+}
+
 int	ft_export(char **args, t_env **env)
 {
 	t_env	*tmp;
 	char	*var;
+	char	*value;
 	int	i;
 
 	i = 1;
 	while (args[i])
 	{
+		expars(args[i]);
 		var = hb_substr(args[i], 0, beforequ(args[i]));
-		tmp = envnew(var, args[i] + beforequ(args[i]) + 1);
-		envadd_back(env, tmp);
+		value = args[i] + beforequ(args[i]) + 1;
+		if (!*value)
+			value = "\0";
+		if (getvalue(*env, var) != NULL)
+			editvar(env, var, value);
+		else
+		{
+			tmp = envnew(var, value);
+			envadd_back(env, tmp);
+		}
 		i++;
 	}
 	return (0);
