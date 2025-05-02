151451b2a5984bb19e37a60f8ce1d3c14d258275
yassine <deepseeko@kali>
2025-05-02 12:56:46 -0400

    fix parser (l9it 3ando 39al debana kay39al 4i 3la a5ir WORD )

--- parser.c
+++ parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybouanan <ybouanan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: deepseeko <deepseeko@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 02:20:00 by ybouanan          #+#    #+#             */
-/*   Updated: 2025/04/27 17:46:17 by ybouanan         ###   ########.fr       */
+/*   Updated: 2025/05/02 12:52:36 by deepseeko        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,64 +15,79 @@
 
 int	fill_redir(t_cmd *cmd, t_token *tok);
 
-static void	parse_args(t_cmd *cmd, t_token **tok)
+static void add_arg_to_cmd(t_cmd *cmd, char *value)
 {
-	int	argc;
-	int	i;
+	int argc = 0;
+	int i = 0;
+	char **new_args;
 
-	argc = count_args(*tok);
-	if (argc > 0)
+	if (cmd->args)
+		while (cmd->args[argc])
+			argc++;
+	new_args = (char **)ft_malloc(sizeof(char *) * (argc + 2));
+	if (cmd->args)
 	{
-		cmd->args = tokens_to_args(*tok, argc);
-		i = 0;
-		while (i < argc && *tok)
+		while (cmd->args[i])
 		{
-			*tok = (*tok)->next;
+			new_args[i] = cmd->args[i];
 			i++;
 		}
 	}
+	new_args[i++] = value;
+	new_args[i] = NULL;
+	cmd->args = new_args;
 }
 
-static void	parse_redirs(t_cmd *cmd, t_token **tok)
+static void handle_token(t_cmd *cmd, t_token *tok, int *redir_pending)
 {
-	while (*tok && is_redir((*tok)->type))
+	if (*redir_pending)
 	{
-		fill_redir(cmd, *tok);
-		*tok = (*tok)->next;
-		if (*tok)
-			*tok = (*tok)->next;
+		add_redirect(&cmd->redirect_list, tok->value, (t_token_type)*redir_pending);
+		*redir_pending = 0;
 	}
+	else
+		add_arg_to_cmd(cmd, tok->value);
 }
 
-static int	is_pipe(t_token *tok)
+static void	handle_token_loop(t_cmd **cmd_curr, t_token **tok, int *redir_pending)
 {
-	return (tok && tok->type == TOKEN_PIPE);
-}
-
-static void	advance_pipe(t_cmd **cmd, t_token **tok)
-{
-	if (is_pipe(*tok))
+	if ((*tok)->type == TOKEN_PIPE)
 	{
-		(*cmd)->next = new_cmd();
-		*cmd = (*cmd)->next;
+		(*cmd_curr)->next = new_cmd();
+		*cmd_curr = (*cmd_curr)->next;
 		*tok = (*tok)->next;
+		*redir_pending = 0;
+		return;
 	}
+	if (is_redir((*tok)->type))
+	{
+		*redir_pending = (*tok)->type;
+		*tok = (*tok)->next;
+		return;
+	}
+	if ((*tok)->type == TOKEN_WORD)
+		handle_token(*cmd_curr, *tok, redir_pending);
+	*tok = (*tok)->next;
 }
 
-t_cmd	*parse_tokens(t_token *tokens)
+static void	parse_tokens_loop(t_cmd *cmd_head, t_token *tokens)
 {
-	t_cmd	*cmd_head;
-	t_cmd	*cmd_curr;
-	t_token	*tok;
+	t_cmd *cmd_curr;
+	t_token *tok;
+	int redir_pending;
 
-	cmd_head = new_cmd();
 	cmd_curr = cmd_head;
 	tok = tokens;
+	redir_pending = 0;
 	while (tok)
-	{
-		parse_args(cmd_curr, &tok);
-		parse_redirs(cmd_curr, &tok);
-		advance_pipe(&cmd_curr, &tok);
-	}
+		handle_token_loop(&cmd_curr, &tok, &redir_pending);
+}
+
+t_cmd *parse_tokens(t_token *tokens)
+{
+	t_cmd *cmd_head;
+
+	cmd_head = new_cmd();
+	parse_tokens_loop(cmd_head, tokens);
 	return (cmd_head);
 }
