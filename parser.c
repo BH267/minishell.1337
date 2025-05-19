66a6b28986d2d29f04725ac7d3ed6fc35c1696d6
deepseeko <yassine44441234@gmail.com>
2025-05-19 20:52:51 +0100

    clean

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: ybouanan <ybouanan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 02:20:00 by ybouanan          #+#    #+#             */
-/*   Updated: 2025/05/19 19:51:11 by ybouanan         ###   ########.fr       */
+/*   Updated: 2025/05/19 20:52:17 by ybouanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -253,39 +253,39 @@ void	clear_quotes(t_token *tokens)
 }
 
 ////  test ////
-static void	print_redirects(t_redirect *redir)
-{
-	while (redir)
-	{
-		printf("  redirect: type=%d, value=", redir->type);
-		if (redir->value)
-			printf("%s\n", redir->value);
-		else
-			printf("(null)\n");
-		redir = redir->next;
-	}
-}
+// static void	print_redirects(t_redirect *redir)
+// {
+// 	while (redir)
+// 	{
+// 		printf("  redirect: type=%d, value=", redir->type);
+// 		if (redir->value)
+// 			printf("%s\n", redir->value);
+// 		else
+// 			printf("(null)\n");
+// 		redir = redir->next;
+// 	}
+// }
 
-static void	print_cmds(t_cmd *cmd)
-{
-	int	i;
+// static void	print_cmds(t_cmd *cmd)
+// {
+// 	int	i;
 
-	while (cmd)
-	{
-		printf("Command:\n");
-		if (cmd->args)
-		{
-			i = 0;
-			while (cmd->args[i])
-			{
-				printf("  arg[%d]: %s\n", i, cmd->args[i]);
-				i++;
-			}
-		}
-		print_redirects(cmd->redirect_list);
-		cmd = cmd->next;
-	}
-}
+// 	while (cmd)
+// 	{
+// 		printf("Command:\n");
+// 		if (cmd->args)
+// 		{
+// 			i = 0;
+// 			while (cmd->args[i])
+// 			{
+// 				printf("  arg[%d]: %s\n", i, cmd->args[i]);
+// 				i++;
+// 			}
+// 		}
+// 		print_redirects(cmd->redirect_list);
+// 		cmd = cmd->next;
+// 	}
+// }
 
 
 t_cmd	*parse_tokens(t_token *tokens, t_env *env)
